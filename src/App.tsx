9bbaa4dbf73bf371e356d3fcae4be25cd84caa2e
Guilherme Oliveira <guilhermefrancooliveira1994@gmail.com>
2026-01-21 22:46:40 -0300

    chore: remove VLibras accessibility widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import Home from './pages/Home';
-import { VLibras } from './components/VLibras';
 
 function App() {
   return (
     <div className="min-h-screen w-full overflow-x-hidden">
-      <VLibras />
       <Home />
     </div>
   );
